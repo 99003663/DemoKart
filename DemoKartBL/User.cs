6292fb1e4732fba7bfaf99d5c7dbbee894a8110e
99003675 <78866490+99003675@users.noreply.github.com>
2021-02-24 03:21:17 -0800

    Update User.cs

--- DemoKartBL/User.cs
+++ DemoKartBL/User.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartBL
+namespace MiniProject
 {
-    class User
+    public class User
     {
+        public string username  { get; set; }
+        public string address { get; set; }
+        public int userid  { get; set; }
+        public int mobileno  { get; set; }
+        public int password  { get; set; }
+
+        public User(string username,string address,int userid,int mobileno,int password)
+        {
+            this.username = username;
+            this.address = address;
+            this.userid = userid;
+            this.mobileno= mobileno ;
+            this.password = password;
+        }
+        
+        public void display()
+        {
+            Console.WriteLine("Name is:" +this.username);
+            Console.WriteLine("Address is:" + this.address);
+            Console.WriteLine("Id is:" + this.userid);
+            Console.WriteLine("No is:" + this.mobileno);
+        }
+
+
+
     }
 }
+
