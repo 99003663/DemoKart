44ff8a2df9133ca4b4e86ea4a51d467aad332e31
99003675 <78866490+99003675@users.noreply.github.com>
2021-02-24 20:58:37 -0800

    Update Category.cs

--- DemoKartBL/Category.cs
+++ DemoKartBL/Category.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace DemoKartBL
 {
-    class Category
+    public class Category
     {
+        
+
+        public String categories;
+        public Category()
+        {
+            categories = null;
+        }
+        public Category(String categories)
+        {
+            this.categories = categories;
+        }
+        public double CalculatePrice(int type, double price)
+        {
+            if (type == 1)
+            {
+                double totalPrice = 0;
+                totalPrice = price + (price * 0.18);
+                return totalPrice;
+            }
+            else if (type == 2)
+            {
+                double totalPrice = 0;
+                totalPrice = price + (price * 0.04);
+                return totalPrice;
+            }
+            else if (type == 3)
+            {
+                double totalPrice = 0;
+                totalPrice = price + (price * 0.07);
+                return totalPrice;
+            }
+            else if (type == 4)
+            {
+                double totalPrice = 0;
+                totalPrice = price + (price * 0.18);
+                return totalPrice;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
     }
 }
