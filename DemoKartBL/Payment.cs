49c530cb006a55bcf014a4c36f4fc04a150919f4
99003663 <78896013+99003663@users.noreply.github.com>
2021-02-25 14:50:54 +0530

    Update Payment.cs

--- DemoKartBL/Payment.cs
+++ DemoKartBL/Payment.cs
@@ -1,12 +1,53 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartBL
+namespace ShoppingCartPro
 {
     class Payment
     {
+        enum Mode
+        {
+            Card = 1,
+            Cash = 2
+        }
+        public double CGSTTax(int Number, double price)
+        {
+            if (Number == (int)Mode.Card)
+            {
+                double Price = 0;
+                Price = price * 0.18;
+                return Price;
+
+            }
+            else
+            {
+                double Price = 0;
+                Price = Price * 0.035 * 0.18;
+                return Price;
+            }
+        }
+        public double SGSTTax(int Number, double price)
+        {
+            if (Number == (int)Mode.Cash)
+            {
+                double Price = 0;
+                Price = Price + 0;
+                return Price;
+
+            }
+            else
+            {
+                double Price = 0;
+                Price = price + (price * 0.035);
+                return Price;
+            }
+
+        }
     }
 }
+
+
+
