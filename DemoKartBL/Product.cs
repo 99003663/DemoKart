3827393fe62d39e2a161f83362aea13b38bce007
99003675 <78866490+99003675@users.noreply.github.com>
2021-02-24 20:57:59 -0800

    Update Product.cs

--- DemoKartBL/Product.cs
+++ DemoKartBL/Product.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartBL
+namespace MiniProject
 {
-    class Product
+    public class Product
     {
+        public int Id;
+        public string Name;
+        public int Price;
+        public int Quantity;
+
+        public Product()
+        {
+            Id = 0;
+            Name = null;
+            Price = 0;
+            Quantity = 0;
+        }
+
+        public Product(int Id, string Name, int Price, int Quantity)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Price = Price;
+            this.Quantity = Quantity;
+
+        }
+
+
     }
 }
