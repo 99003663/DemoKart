47b35c3bd3682ec733c840f529d6824552cf830f
99003663 <78896013+99003663@users.noreply.github.com>
2021-02-25 23:07:39 +0530

    Update Order.cs

--- DemoKartBL/Order.cs
+++ DemoKartBL/Order.cs
@@ -1,12 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartBL
+namespace ECommerceSite
 {
-    class Order
-    {
+    public class Order : User
+        {
+            public String deliveryAddress;
+            public int pinCode;
+
+        }
     }
-}
