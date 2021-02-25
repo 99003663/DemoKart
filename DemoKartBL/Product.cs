1532f2d73b8763bce9244890c6171e15d606f717
99003663 <78896013+99003663@users.noreply.github.com>
2021-02-25 23:09:20 +0530

    Update Product.cs

--- DemoKartBL/Product.cs
+++ DemoKartBL/Product.cs
@@ -4,32 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiniProject
+namespace ECommerceSite
 {
-    public class Product
-    {
-        public int Id;
-        public string Name;
-        public int Price;
-        public int Quantity;
-
-        public Product()
+        public class Product
         {
-            Id = 0;
-            Name = null;
-            Price = 0;
-            Quantity = 0;
+            public int productId { get; set; }
+            public String productName { get; set; }
+            public int quantity { get; set; }
+            public double price { get; set; }
+
+
+            public Category CObj = new Category();
+              public Product()
+            {
+                productId = 0;
+                productName = null;
+                quantity = 0;
+                price = 0;
+            }
+            public Product(int productId, String productName, double price, int quantity, int CategoriesId, String Categories)
+            {
+                this.productId = productId;
+                this.productName = productName;
+                this.quantity = quantity;
+                this.price = price;
+                CObj.categoriesId = CategoriesId;
+                CObj.categories = Categories;
+            }
         }
-
-        public Product(int Id, string Name, int Price, int Quantity)
-        {
-            this.Id = Id;
-            this.Name = Name;
-            this.Price = Price;
-            this.Quantity = Quantity;
-
-        }
-
-
     }
-}
