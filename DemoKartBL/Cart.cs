081db7f8eaa2a5c778156e72310210c527df280a
99003663 <78896013+99003663@users.noreply.github.com>
2021-02-25 23:05:35 +0530

    Update Cart.cs

--- DemoKartBL/Cart.cs
+++ DemoKartBL/Cart.cs
@@ -1,18 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartBL
+namespace ECommerceSite
 {
-    public class Cart
-    {
-        //Delete this method before submission
-        //Method was created to generate test project
-        public void SampleMethod()
+        public class Cart
         {
+            public int cartId;
 
+
+            public int AddToCart(int Quantity)
+            {
+                if (Quantity > 10)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+
+            }
         }
     }
-}
