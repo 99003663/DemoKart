539f4c7ad447c4097dc60fddb2b6c3205add0be6
99003663 <78896013+99003663@users.noreply.github.com>
2021-02-25 23:06:22 +0530

    Update Category.cs

--- DemoKartBL/Category.cs
+++ DemoKartBL/Category.cs
@@ -4,52 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoKartBL
+namespace ECommerceSite
 {
-    public class Category
-    {
-        
+        public class Category
+        {
+            //enum Categories {electronics=1, grocery, clothing, cosmetic }
 
-        public String categories;
-        public Category()
-        {
-            categories = null;
-        }
-        public Category(String categories)
-        {
-            this.categories = categories;
-        }
-        public double CalculatePrice(int type, double price)
-        {
-            if (type == 1)
-            {
-                double totalPrice = 0;
-                totalPrice = price + (price * 0.18);
-                return totalPrice;
-            }
-            else if (type == 2)
-            {
-                double totalPrice = 0;
-                totalPrice = price + (price * 0.04);
-                return totalPrice;
-            }
-            else if (type == 3)
-            {
-                double totalPrice = 0;
-                totalPrice = price + (price * 0.07);
-                return totalPrice;
-            }
-            else if (type == 4)
-            {
-                double totalPrice = 0;
-                totalPrice = price + (price * 0.18);
-                return totalPrice;
-            }
-            else
-            {
-                return 0;
-            }
-        }
+            //public String categories;
+            //public int categoriesId;
 
+            public int categoriesId { get; set; }
+            public String categories { get; set; }
+
+            public double totalPrice = 0;
+            public Category()
+            {
+                categories = null;
+            }
+            public Category(int categoriesId, String categories)
+            {
+                this.categories = categories;
+                this.categoriesId = categoriesId;
+            }
+
+
+            public double CalculatePrice(int type, double price, int Quantity)
+            {
+                if (type == 1)
+                {
+                    price = price * Quantity;
+                    totalPrice = price + (price * 0.18);
+                    return totalPrice;
+                }
+                else if (type == 2)
+                {
+                    price = price * Quantity;
+                    totalPrice = price + (price * 0.04);
+                    return totalPrice;
+                }
+                else if (type == 3)
+                {
+                    price = price * Quantity;
+                    totalPrice = price + (price * 0.07);
+                    return totalPrice;
+                }
+                else if (type == 4)
+                {
+                    price = price * Quantity;
+                    totalPrice = price + (price * 0.18);
+                    return totalPrice;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+        }
     }
-}
