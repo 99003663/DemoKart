fb23e11ebd7a10751d3abc4594e91da5256b7b5f
99003663 <78896013+99003663@users.noreply.github.com>
2021-03-06 14:25:21 +0530

    Update Payment.cs

--- DemoKartBL/Payment.cs
+++ DemoKartBL/Payment.cs
@@ -3,16 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ECommerceSite
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    namespace DemoKartBL
+namespace DemoKartBL
     {
         public class Payment
         {
